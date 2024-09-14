0e88945e2ce875af33bccab6d3625b33d3522d3f
kenresoft <amadikenneth8@gmail.com>
2024-09-14 21:20:03 +0100

    Modified config/database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,36 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error(error.message);
         process.exit(1);
     }
+};
+
+module.exports = connectDB;
+ */
+
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+let db;
+
+const connectDB = async () => {
+    if (!db) {
+        await client.connect();
+        db = client.db(process.env.DB_NAME);
+    }
+    return db;
 };
 
 module.exports = connectDB;
